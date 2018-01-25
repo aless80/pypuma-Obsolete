8a8bbf09d44a20f570244ea1d0546f8188b0615d
Alessandro <AlessandroMarin80@gmail.com>
2018-01-25 07:10:40 -0500

    Thu Jan 25 07:10:40 EST 2018

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ from setuptools import setup, find_packages
 setup(name='pypuma',
     version='0.1',
     description='bla.',
-    url='https://github.com/aless80/PyPuma',
+    url='https://github.com/aless80/pypuma',
     author='Alessandro Marin',
     author_email='AlessandroMarin80@gmail.com',
     license='MIT',
-    packages=['pypanda'],
+    packages=['pypuma'],
     install_requires=['pandas',
     'numpy',
     'networkx',
     'matplotlib'
     ],
-    scripts=['bin/pypanda'],
+    scripts=['bin/pypuma'],
     zip_safe=False)
